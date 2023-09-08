11289b4267b9028fd03a834be24d989ed28b60f1
samirm00 <sammoham.be@gmail.com>
2023-09-08 07:38:30 +0200

    initial commit

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react';
+
 import './App.css';
 import Button from './components/Button';
 
 const App = () => {
-    const num = 0;
-    // const [num, setNum] = useState(0);
+    const [count, setCount] = useState(0);
     const clickHandler = () => {
-        // setNum(num + 1);
+        setCount((previousCount) => previousCount + 1);
     };
 
     return (
         <div>
-            <h1>{num}</h1>
+            <h1>{count}</h1>
             <Button clickHandler={clickHandler} />
         </div>
     );
